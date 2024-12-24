6f27ddac9d5de4db3c9dda3b2a529f16f799e4d9
RakhKaran <132744751+RakhKaran@users.noreply.github.com>
2024-12-24 14:08:53 +0530

    "firebase app multiple initialization issue solved"

--- API/src/services/firebase.service.ts
+++ API/src/services/firebase.service.ts
@@ -2,24 +2,31 @@ import { HttpErrors } from '@loopback/rest';
 import * as admin from 'firebase-admin';
 
 export class FirebaseAdmin {
-  constructor() {
-    admin.initializeApp();
-  }
-  
-  async verifyCustomer(token : string):Promise<{message : string, phoneNumber: string}>{
-    try{
-      const decodeToken = await admin.auth().verifyIdToken(token);
-      const { phone_number } = decodeToken;
+  private firebaseApp: admin.app.App;
 
-      if(phone_number){
-        return{
-          message : 'user phone number',
-          phoneNumber : phone_number
-        }
+  constructor() {
+    // Check if a Firebase app is already initialized
+    if (!admin.apps.length) {
+      this.firebaseApp = admin.initializeApp();
+    } else {
+      this.firebaseApp = admin.app(); // Use existing app instance
+    }
+  }
+
+  async verifyCustomer(token: string): Promise<{ message: string; phoneNumber: string }> {
+    try {
+      const decodedToken = await this.firebaseApp.auth().verifyIdToken(token);
+      const { phone_number } = decodedToken;
+
+      if (phone_number) {
+        return {
+          message: 'User phone number',
+          phoneNumber: phone_number,
+        };
       }
 
       throw new HttpErrors.BadRequest('Invalid Token');
-    }catch(error){
+    } catch (error) {
       throw error;
     }
   }
